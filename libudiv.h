f08976106861bc9e71dced0af1be2ccb385fff69
Paul Cercueil <paul@crapouillou.net>
2024-03-14 01:47:30 +0100

    Support creating dividers at compile time
    Do things a little bit differently when the divider value is a known
    constant, so that the resulting udiv_t is also a known compile-time
    constant.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- libudiv.h
+++ libudiv.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (C) 2023 Paul Cercueil <paul@crapouillou.net>
+ * Copyright (C) 2024 Paul Cercueil <paul@crapouillou.net>
  */
 
 #ifndef __LIBUDIV_H__
@@ -17,13 +17,29 @@ typedef struct {
     unsigned int m;
 } udiv_t;
 
-static inline udiv_t udiv_set_divider(unsigned int div)
+#define UDIV_P(div) \
+	(31 - clz32(div) + !!((div) & ((div) - 1)))
+
+#define UDIV_M(div, p) \
+	(unsigned int)(((0x1ull << (32 + (p))) + (div) - 1) / (unsigned long long)(div))
+
+
+static inline udiv_t __udiv_set_divider(unsigned int div)
 {
-    unsigned int p = 31 - clz32(div) + !!(div & (div - 1));
-    unsigned int m = ((0x1ull << (32 + p)) + div - 1) / (unsigned long long)div;
+    unsigned int p = UDIV_P(div);
+    unsigned int m = UDIV_M(div, p);
 
     return (udiv_t){ .p = p, .m = m, };
 }
+
+#ifdef _MSC_BUILD
+#define udiv_set_divider(div) __udiv_set_divider(div)
+#else
+#define udiv_set_divider(div) \
+	__builtin_choose_expr(__builtin_constant_p(div), \
+			      (udiv_t){ .p = UDIV_P(div), .m = UDIV_M((div), UDIV_P(div)) }, \
+			      __udiv_set_divider(div))
+#endif
 
 static inline unsigned int udiv_divide(unsigned int val, udiv_t udiv)
 {
